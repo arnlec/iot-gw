56e44f1f3d0d677ee021eacb07e3c5c6e96d744e
Arnaud Le Cann <arnaud.le.cann@ericsson.com>
2020-04-09 11:59:21 +0200

    Fix invalid value for license classifier

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-        'License :: OSI Approved :: GPLv3'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
     ],
     python_requires='>=3'
 
